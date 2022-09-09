b6fb6b258fe51c38c2865c79ea336ee9a0517a56
AlexVerrilo <alex.verrilo@gmail.com>
2022-09-08 21:12:48 -0300

    Correção de login

--- src/routers/autenticacoes-router.ts
+++ src/routers/autenticacoes-router.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const AutenticacoesRouter = express.Router()
 
-AutenticacoesRouter.post('/autenticacaos', (req,res) => {
+AutenticacoesRouter.post('/autenticacao', (req,res) => {
   if (req.body.email==='admin' && req.body.senha==='admin') {
     res.status(201).json({
         token: '1'
